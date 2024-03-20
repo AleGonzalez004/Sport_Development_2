402a2c37a30509ec6f328e57273c90fe59d66646
KKahunaGames <156387015+KKahunaGames@users.noreply.github.com>
2024-03-19 22:11:28 -0600

    Comando

--- Controladores/Publico/navegacion_cartas.js
+++ Controladores/Publico/navegacion_cartas.js
@@ -1,4 +1,4 @@
     // Navegacion  de las cartas en el catalogo.
     function cartazapatonike() {
-        window.location.href = "/Vistas/Publico/informacion_producto.html";
+        window.location.href = "/Vistas/Publico/Productos/informacion_producto1.html";
     }
