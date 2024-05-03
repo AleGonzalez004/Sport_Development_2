13071d028674134ca4b749ff04e0152d096071ac
AleGonzalez002 <138949757+AleGonzalez002@users.noreply.github.com>
2024-05-03 11:14:20 -0600

    bonobon

---
+++ Recursos/JavaScript/Administradores/Navegacion.js
@@ -0,0 +1,86 @@
+function Navbar() {
+    const Menu =
+    `<nav class="navbar navbar-expand-lg" style="background-color: #245C9D;" id="Navbar">
+        <div class="container-fluid">
+            <img src="/Recursos/Imagenes/logo_blanco.png" class="border-0" alt="..."
+                style="height: 45px; width: 45px; background-color: #245C9D;">
+            <button class="navbar-toggler border-0 text-white" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="bi bi-list text-white"></span>
+            </button>
+            <div class="collapse navbar-collapse  text-white" id="navbarNav">
+                <ul class="navbar-nav  text-white">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white px-4" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-tags-fill"></i> Categorias
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#" onclick="Categorias()">Categorias</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="Deportes()">Deportes</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="Generos()">Generos</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="Imagenes()">Imagenes</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="Marcas()">Marcas</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="Tallas()">Tallas</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white px-4" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-people-fill"></i> Usuarios
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#" onclick="Clientes()">Clientes</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="Clientes()">Administradores</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-white px-4" href="#" onclick="Productos()"><i class="bi bi-box-seam-fill"></i> Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-white px-4" href="#" onclick="Graficos()"><i class="bi bi-pie-chart-fill"></i> Graficos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-white px-4" href="#" onclick="Comentarios()"><i class="bi bi-chat-left-text-fill"></i>
+                            Comentarios</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>`;
+    return Menu;
+}
+    function Categorias() {
+        window.location.href = "/Vistas/Administradores/categoria.html";
+    }
+    function Deportes() {
+        window.location.href = "/Vistas/Administradores/deporte.html";
+    }
+    function Generos() {
+        window.location.href = "/Vistas/Administradores/genero.html";
+    }
+    function Imagenes() {
+        window.location.href = "/Vistas/Administradores/imagen.html";
+    }
+    function Marcas() {
+        window.location.href = "/Vistas/Administradores/marca.html";
+    }
+    function Tallas() {
+        window.location.href = "/Vistas/Administradores/talla.html";
+    }
+    function Clientes() {
+        window.location.href = "/Vistas/Administradore/cliente.html";
+    }
+    function Administradores() {
+        window.location.href = "/Vistas/Administradores/cliente.html";
+    }
+    function Productos() {
+        window.location.href = "/Vistas/Administradores/producto.html";
+    }
+    function Graficos() {
+        window.location.href = "/Vistas/Administradores/grafico.html";
+    }
+    function Comentarios() {
+        window.location.href = "/Vistas/Administradores/comentario.html";
+    }
