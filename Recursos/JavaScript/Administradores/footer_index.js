0cf80d02612df917a7c27e0eb7777280bf04c803
AleGonzalez002 <138949757+AleGonzalez002@users.noreply.github.com>
2024-05-09 15:06:36 -0600

    :c

---
+++ Recursos/JavaScript/Administradores/footer_index.js
@@ -0,0 +1,31 @@
+// Pie de pagina de la pagina principal.
+
+function FooterIndex() {
+    const MenuL =
+        `<footer class="container-fluid bg-dark text-white text-center">
+    <div class="row align-items-center">
+        <div class="col-md-4 text-end">
+            <h2 style="margin-right: 20px;">Sport Development</h2>
+        </div>
+        <div class="col-md-4 text-center">
+            <p class="text-justify" style="font-size: 15px;">A través del deporte, tenemos el poder de
+                cambiar vidas. El deporte nos mantiene en forma. Nos mantienen atentos y nos unen. Los atletas
+                nos inspiran. Nos ayudan a levantarnos y movernos. Y los artículos deportivos con las últimas
+                tecnologías nos
+                ayudan a batir nuestra mejor marca personal.</p>
+        </div>
+        <div class="col-md-4 text-start">
+            <div class="social-icons text-start">
+                <a href="https://www.instagram.com/"><i class="bi bi-instagram fs-3 px-2 text-white"></i></a>
+                <a href="https://www.facebook.com/"><i class="bi bi-facebook fs-3 px-2 text-white"></i></a>
+                <a href="https://www.tiktok.com/"><i class="bi bi-tiktok fs-3 px-2 text-white"></i></a>
+                <a href="https://www.youtube.com/"><i class="bi bi-youtube fs-3 px-2 text-white"></i></i></a>
+                <p style="font-size: 10px; margin-left: 20px;">2024 Sport Development, San Salvador, El
+                    Salvador.</p>
+            </div>
+        </div>
+    </div>
+</footer>`;
+
+    return MenuL;
+}
